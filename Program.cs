343bce9814e50d6eb945149d6fcc77b0d8eaa167
Abhishek-tara <abhishektara.ak47@gmail.com>
2023-02-10 14:16:02 +0530

    Player 1 starting Position added

--- Program.cs
+++ Program.cs
@@ -5,6 +5,12 @@
         public static void Main(String[] args)
         {
             System.Console.WriteLine("Welcome to the Snake and Ladder Game");
+
+            int position = 0;
+
+            Console.WriteLine("Welcome Player 1, Let's start the Game");
+            Console.WriteLine("Player 1 is at position : " + position);
+
         }
     }
 }

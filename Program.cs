bd903ad5031972d48dd1cd37e11d8147f5a62f2b
Abhishek-tara <abhishektara.ak47@gmail.com>
2023-02-10 18:52:32 +0530

    Added Repeat condition till player reaches 100t position

--- Program.cs
+++ Program.cs
@@ -12,27 +12,37 @@
             Console.WriteLine("Player 1 is at position : " + position);
 
             Random random= new Random();
-            int dice = random.Next(1,7);
-
-            Console.WriteLine($"The Player rolls a dice and gets: {dice}");
-
-            int option = random.Next(0, 3);
-
-            if (option == 0)
+            
+            while(position != 100)
             {
-                Console.WriteLine("Player stays at same Position");
+                int dice = random.Next(1, 7);
+
+                Console.WriteLine($"The Player rolls a dice and gets: {dice}");
+
+                int option = random.Next(0, 3);
+
+                if (option == 0)
+                {
+                    Console.WriteLine("Player stays at same Position");
+                }
+                else if (option == 1)
+                {
+                    Console.WriteLine("Player Moves Ahead by {0} from ladder", dice);
+                    position += dice;
+                }
+                else
+                {
+                    Console.WriteLine("Player Moves Behind by {0} from Snake", dice);
+                    position -= dice;
+                }
+                if(position < 0)
+                {
+                    position = 0;
+                }
+                Console.WriteLine("Player Current Position is at : {0}", position);
+
             }
-            else if (option == 1)
-            {
-                Console.WriteLine("Player Moves Ahead by {0} from ladder", dice);
-                position += dice;
-            }
-            else
-            {
-                Console.WriteLine("Player Moves Behind by {0} from Snake", dice);
-                position -= dice;
-            }
-            Console.WriteLine("Current Position is at : {0}", position);
+            Console.WriteLine("Player is at Position : {0}", position);
 
         }
     }

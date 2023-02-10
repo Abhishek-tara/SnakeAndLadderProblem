556574d67a25d4e6234de7250a8fd62313b44adc
Abhishek-tara <abhishektara.ak47@gmail.com>
2023-02-10 18:38:28 +0530

    Player1 checks for options No play,ladder or snake

--- Program.cs
+++ Program.cs
@@ -16,6 +16,24 @@
 
             Console.WriteLine($"The Player rolls a dice and gets: {dice}");
 
+            int option = random.Next(0, 3);
+
+            if (option == 0)
+            {
+                Console.WriteLine("Player stays at same Position");
+            }
+            else if (option == 1)
+            {
+                Console.WriteLine("Player Moves Ahead by {0} from ladder", dice);
+                position += dice;
+            }
+            else
+            {
+                Console.WriteLine("Player Moves Behind by {0} from Snake", dice);
+                position -= dice;
+            }
+            Console.WriteLine("Current Position is at : {0}", position);
+
         }
     }
 }
